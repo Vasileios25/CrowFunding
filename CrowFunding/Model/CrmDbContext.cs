8bc04481bccf3a6152ed31a7f4fab7689a7d0500
vasil <billys01101@gmail.com>
2021-11-27 07:54:15 +0200

    Add project files.

---
+++ CrowFunding/Model/CrmDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrowFunding.Model
+{
+    public class CrmDbContext : DbContext
+    {
+
+
+
+
+
+
+
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+          optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=RegenCrm2;User ID=sa;Password=admin!@#123");
+        }
+    }
+}
